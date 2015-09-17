9d0321d1ba78cd046b34d125a79865ce42b4be13
Trevor Helms <thelms501@gmail.com>
2015-09-17 19:21:59 -0400

    Cleanup nodetest script, make a gitignore file

--- VeServerTest/nodetest.js
+++ VeServerTest/nodetest.js
@@ -2,11 +2,6 @@
 var http = require('http')
 http.post = require('http-post')
 
-var url = 'localhost:4567'		// Ve server runs on 4567 (maybe change that in production?)
-var language = 'ja'				// Don't think we'll need English parsing?
-
-var data = []
-
 var text = 'こんにちは！元気ですか。私はトレバー・ヘルムズと言います。日本語とコンピュータを勉強している学生です。'
 
 var data = {
