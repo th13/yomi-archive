6f3744e4031c1a9b0b985fff9558e0d023c94e79
Trevor Helms <thelms501@gmail.com>
2015-12-03 21:35:50 -0500

    Add analyze page

--- app/assets/javascripts/popover.js
+++ app/assets/javascripts/popover.js
@@ -1,12 +1,12 @@
 'use strict';
 
-$(function () {
-	// This handy trick makes the links essentially not links.
-	var def;
+$(".main.analyze").ready(function() {
+  // This handy trick makes the links essentially not links.
+  var def;
 
-	$('a.jp').click(function(e) {
-		e.preventDefault();
-	});
+  $('a.jp').click(function(e) {
+    e.preventDefault();
+  });
 
-	$('[data-toggle="popover"]').popover();
+  $('[data-toggle="popover"]').popover();
 });
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -11,6 +11,21 @@ class MainController < ApplicationController
   end
 
   def analyze
+    # TODO We probably should add a filter that gets the correct definition!!!
+    @words = Ve.in('ja').words('これは本当に何もじゃないです。')
+    @out = []
+    @words.each do |word|
+      if word.part_of_speech != Ve::PartOfSpeech::Symbol
+        search = URI(URI.encode('http://jisho.org/api/v1/search/words?keyword=' + word.lemma))
+        res = JSON.parse(Net::HTTP.get(search))
+        defin = res['data'][0]['senses'][0]['english_definitions'][0]
+        @out.push({ jp: word.word, def: defin, pos: word.part_of_speech.name })
+      else
+        @out.push({ jp: word.word, def: '', pos: word.part_of_speech.name })
+      end
+    end
+
+    puts @out
   end
 
   def new
